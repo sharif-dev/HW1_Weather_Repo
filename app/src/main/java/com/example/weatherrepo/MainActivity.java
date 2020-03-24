74ca953a29cc3d13aa57a1bbf046f0d148c0fa30
esmaeil1377 <pahang1378@gmail.com>
2020-03-24 22:40:45 +0430

    add editText and Button to MainActivity

--- app/src/main/java/com/example/weatherrepo/MainActivity.java
+++ app/src/main/java/com/example/weatherrepo/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.weatherrepo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.edit_query_city);
+
+        Button button = (Button) findViewById(R.id.button_city);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("EditText", editText.getText().toString());
+            }
+        });
     }
 }
